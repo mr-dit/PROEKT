9c739f1de729bb2b460f3e347a736c94e09225e9
mr-dit <mezentsev000@mail.ru>
2023-05-10 22:54:44 +0600

    2 commit

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose');
 const router = require('./router/index')
 const errorMiddleware = require('./middlewares/error-middleware');
+const path = require('path')
+
 
 const PORT = process.env.PORT || 5000;
 const app = express()
@@ -17,6 +19,14 @@ app.use(cors({
 }));
 app.use('/api', router);
 app.use(errorMiddleware);
+
+if (true){
+    app.use('/', express.static(path.join(__dirname, 'clint', 'build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'clint', 'build', 'index.html'))
+    })
+}
 
 const start = async () => {
     try {
